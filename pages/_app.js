4bb58345d9d51e8c3dda2df8e472e83194df54ed
s1rbl4ck <danesh.hb82@gmail.com>
2024-02-24 22:06:48 +0100

    Added next.js progress bar to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { Roboto_Slab } from "next/font/google";
+import NextNProgress from 'nextjs-progressbar';
 
 const font = Roboto_Slab({ subsets: ["latin"], variable: "--font-slab" });
 
 export default function App({ Component, pageProps }) {
     return (
         <NextUIProvider className={font.className}>
+            <NextNProgress color="#e50914" startPosition={0.3} stopDelayMs={200} height={4} showOnShallow={true} />
             <Component {...pageProps} />
         </NextUIProvider>
     );
